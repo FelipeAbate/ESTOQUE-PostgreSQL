e9bdc4d184bd63c34e620b7556a66962e74754c9
Felipe Abate <felipeabate2017@gmail.com>
2024-06-14 06:19:22 -0300

    método DELETE sendo chamado recebendo o id para ser DELETADO

--- src/main/java/com/estoque/App.java
+++ src/main/java/com/estoque/App.java
@@ -1,28 +1,45 @@
 package com.estoque;
 
 import java.sql.Connection;
-import java.sql.DriverManager;   // Gerenciador de Driver
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 public class App {
 
      public static void main (String[] args) {
-         try {
-             Connection conexcao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/produtos",
-                     "postgres", "senha123");
-             if (conexcao != null) {
-                 System.out.println("\n\n Banco de dados conectado com sucesso!");
-                 
-             } else {
-                 System.out.println("Conexão falhou!");
+        try {
+            Connection conexcao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/produtos",
+                    "postgres", "senha123");
+            if (conexcao != null) {
+                System.out.println("\n\n Banco de dados conectado com sucesso!");
+                
+            } else {
+                System.out.println("Conexão falhou!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        InsertSQL insertInCamisetas = new InsertSQL();
+        insertInCamisetas.insert_sql();
+        insertInCamisetas.insert_sql();
+        insertInCamisetas.insert_sql();
+
+         DeleteSQL deleteId = new DeleteSQL();
+         deleteId.delete_sql(55); 
+
+         
+         /* 
+          Como deletar varios: 
+
+          funciona pq os valores a deletar são int
+          já que o for é feito com int i
+          Ex:
+
+             for (int i = 21; i <= 52; i++){
+             deleteId.delete_sql(i);
              }
-         } catch (SQLException e) {
-             e.printStackTrace();
-         }
-        
-         InsertSQL insertInCamisetas = new InsertSQL();
-         insertInCamisetas.insert_sql();
+         */
 
      }
 }
